0c21590474f51087b05199d33634a968f7a909fd
Addit <adam.garza33@hotmail.com>
2023-06-01 18:30:31 -0500

    imported thought and user module and defined thoughtController Object in thoughtController.js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,17 @@
+const thoughtController = {
+    // get all Thoughts
+    getAllThought(req, res) {
+      Thought.find({})
+        .populate({
+          path: "reactions",
+          select: "-__v",
+        })
+        .select("-__v")
+        .sort({ _id: -1 })
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+          console.log(err);
+          res.sendStatus(400);
+        });
+    },
+};
