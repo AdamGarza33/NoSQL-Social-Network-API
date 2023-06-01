4b94389b5964702c8000cd48d07356c9d08e176b
Addit <adam.garza33@hotmail.com>
2023-06-01 15:25:31 -0500

    imported user controller functions

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+  } = require("../../controllers/user-controller");
+  
