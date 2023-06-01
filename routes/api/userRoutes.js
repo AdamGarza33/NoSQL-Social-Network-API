13c5b85b8677058a3beb5f57be2f27dbe5e6df5c
Addit <adam.garza33@hotmail.com>
2023-06-01 15:33:06 -0500

    added routes for /friends/:friendId in userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -16,3 +16,9 @@ router.route("/").get(getAllUser).post(createUser);
 
 // Define the routes for /api/users/:id
 router.route("/:id").get(getUserById) .put(updateUser) .delete(deleteUser); 
+
+// Define the routes for /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+// Export the router to be used in other parts of the app
+module.exports = router;
