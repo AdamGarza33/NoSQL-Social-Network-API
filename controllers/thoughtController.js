1c9574d87f2e5f00f86938275f170a8fa9ee03f3
Addit <adam.garza33@hotmail.com>
2023-06-01 18:34:40 -0500

    create thought and push to associated user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -40,4 +40,29 @@ const thoughtController = {
         res.sendStatus(400);
       });
   },
+
+   // create Thought
+  // push the created thought's _id to the associated user's thoughts array field
+  createThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "Thought created but no user with this id!" });
+        }
+
+        res.json({ message: "Thought successfully created!" });
+      })
+      .catch((err) => res.json(err));
+  },
+
+
 };
