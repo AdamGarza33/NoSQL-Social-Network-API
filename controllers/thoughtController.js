54e825be1fdcfa3068de3752c907e7d2cb54288f
Addit <adam.garza33@hotmail.com>
2023-06-01 18:33:37 -0500

    retrieve thought by _id, find a single thought by its _id and check if a thought with the provided ID exists

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,7 @@
+// Import the Thought and User models
+const { Thought, User } = require("../models");
+
+// Define the thoughtController object
 const thoughtController = {
     // get all Thoughts
     getAllThought(req, res) {
@@ -14,4 +18,26 @@ const thoughtController = {
           res.sendStatus(400);
         });
     },
+
+    // get one Thought by id
+  getThoughtById({ params }, res) {
+    // Find a single thought by its _id
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbThoughtData) => {
+        // Check if a thought with the provided ID exists
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: "No thought with this id!" });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 };
