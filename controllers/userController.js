f0c9d522dc6a86a3c151f6be5dd0e8dae8abe822
Addit <adam.garza33@hotmail.com>
2023-06-01 20:28:42 -0500

    get on user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,5 +17,31 @@ const userController = {
       });
   },
 
+// get one user by id
+getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .populate({
+        path: "friends",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "No user found with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 
+  
 };
