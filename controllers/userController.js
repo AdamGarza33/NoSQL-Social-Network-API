4faaa0165af77602c263cb1896b1c741f3788a87
Addit <adam.garza33@hotmail.com>
2023-06-01 20:29:32 -0500

    update user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,4 +50,19 @@ getUserById({ params }, res) {
       .catch((err) => res.json(err));
   },
 
+   // update user by id
+   updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
